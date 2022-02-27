567c0895a3dca208c3fb69e72bfe0e54c89d3bd5
Gerry Wolfe <gwolfe@slync.io>
2022-02-27 10:01:38 -0800

    string text

--- src/main.rs
+++ src/main.rs
@@ -1,56 +1,24 @@
-fn median(mut a: Vec<f32>) -> Option<f32> {
-    if a.is_empty() {
-        return None;
-    }
-
-    a.sort_by(|x, y| x.partial_cmp(y).unwrap());
-
-    let n_elements = a.len();
-    let middle = n_elements/2;
-
-    let med = if n_elements % 2 == 0 {
-        (a[middle -1] + a[middle]) / 2.0
-    } else {
-        a[middle]
-    };
-
-    Some(med)
+fn info<T: AsRef<str>>(a: &T) {
+    println!("{}", a.as_ref())
 }
 
 fn main() {
-    let answer: Option<f32> = median(vec![1.0, 2.0, 5.0]);
-    println!("median([1,2,5]) = {:?}", answer);
+    let a = "?";
+    let b = "Hello, linkedin!".to_string();
+    info(&a);
+    info(&b);
+
 
 }
 
 #[test]
-fn empty_list() {
-    let input = vec![];
-    let expected_output = None;
-    let actual_output = median(input);
-    assert_eq!(actual_output, expected_output);
+fn str() {
+    let input = "Rust";
+    info(&input);
 }
 
 #[test]
-fn sorted_list() {
-    let input = vec![1.0, 4.0, 5.0];
-    let expected_output = Some(4.0);
-    let actual_output = median(input);
-    assert_eq!(actual_output, expected_output);
-}
-
-#[test]
-fn even_length() {
-    let input = vec![1.0, 3.0, 5.0, 6.0];
-    let expected_output = Some(4.0);
-    let actual_output = median(input);
-    assert_eq!(actual_output, expected_output);
-}
-
-#[test]
-fn unsorted_list() {
-    let input = vec![1.0, 5.0, 2.0];
-    let expected_output = Some(2.0);
-    let actual_output = median(input);
-    assert_eq!(actual_output, expected_output);
+fn string() {
+    let input = String::from("Rust");
+    info(&input);
 }
